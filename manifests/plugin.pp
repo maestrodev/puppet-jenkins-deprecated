62e9ac566b25034c5ac7fcb68ce899d58d08e353
Etienne Pelletier <epelletier@maestrodev.com>
2012-10-24 12:19:53 -0500

    Fixed some conflicts in plugin directory creation.

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,4 +1,6 @@
 define jenkins::plugin($version=0) {
+  $user              = $jenkins::jenkins_user
+  $group             = $jenkins::jenkins_group
   $plugin            = "${name}.hpi"
   $plugin_dir        = '/var/lib/jenkins/plugins'
   $plugin_parent_dir = '/var/lib/jenkins'
@@ -10,13 +12,23 @@ define jenkins::plugin($version=0) {
     $base_url   = 'http://updates.jenkins-ci.org/latest/'
   }
 
-  if (!defined(File[$plugin_dir])) {
+  if (!defined(File[$plugin_parent_dir])) {
     file {
-      [$plugin_parent_dir, $plugin_dir]:
+      $plugin_parent_dir:
         ensure  => directory,
-        owner   => 'jenkins',
-        group   => 'jenkins',
-        require => [Group['jenkins'], User['jenkins']];
+        owner   => $user,
+        group   => $group,
+        require => [Group[$group], User[$user]];
+    }
+  }
+  
+
+  if (!defined(File[$plugin_dir])) {
+    file { $plugin_dir:
+        ensure  => directory,
+          owner   => $user,
+          group   => $group,
+          require => [Group[$group], User[$user], File[$plugin_parent_dir]];    
     }
   }
 
